1913da4883c614dd539354042219f50fbba7639f
Britoshky <britoshky@gmail.com>
2022-07-19 17:10:38 -0400

    Ajustando

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
+const {Howl, Howler} = require('howler');
 
 
 router.get("/", async (req, res) => {
   res.render("index", { title: "Express" });
+});
+
+router.post("/btn-player", async (req, res) => {
+  const sound = new Howl({
+    src: ['http://audio.streaminghd.cl:9134/stream'],
+    autoplay: true,
+    html5: true,
+    loop: true,
+    volume: 0.5,
+});
+  sound.play();
 });
 
 module.exports = router;

aa1c422b7b9b348de6b7978358ed6df024f69a35
Britoshky <britoshky@gmail.com>
2022-08-30 18:49:19 -0400

    probando

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const passport = require("passport");
 const app = express();
 require("./lib/passport");
 // settings
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
